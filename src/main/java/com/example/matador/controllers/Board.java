fe0e8b512a5a77bed4b63809c4899b002fc8115a
Toby <tobiasgoerlyk@hotmail.com>
2022-12-12 20:36:32 +0100

    Uploaded field icon PNGs

--- src/main/java/com/example/matador/controllers/Board.java
+++ src/main/java/com/example/matador/controllers/Board.java
@@ -1,4 +1,33 @@
 package com.example.matador.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 public class Board {
+
+
+    @FXML
+    private HBox top;
+    @FXML
+    private HBox bottom;
+    @FXML
+    private VBox left;
+    @FXML
+    private VBox right;
+
+    @FXML
+    private VBox startField;
+    @FXML
+    private VBox prisonField;
+    @FXML
+    private VBox goToPrisonField;
+    @FXML
+    private VBox freeParkingField;
+
+
+
+
+
+
 }
